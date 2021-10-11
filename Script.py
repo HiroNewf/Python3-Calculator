67c80db106d5e0b5da59b6db7afba8005d58ab0a
SMRUTI R4NJ4N N4Y4K <56423859+SM8UTI@users.noreply.github.com>
2021-10-11 14:26:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,55 +1,79 @@
-def calculate():
-    operation = input('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
-''')
+from tkinter import *
+def click(event):
+    global scvalue
+    text = event.widget.cget("text")
+    print(text)
+    value = ""
+    if text=="=":
+        if scvalue.get().isdigit():
+            value = int(scvalue.get())
+        else:
+            try:
+                value = eval(screen.get())
+            except Exception as e:
+                print(e)
+                value = "Error"
 
-    # number imputs 
-    number_1 = int(input('Enter the first number: '))
-    number_2 = int(input('Enter the second number: '))
+    elif text!="c":
+        value = scvalue.get()+ text
+
+    scvalue.set(value)
+    screen.update()
+
+root = Tk()
+root.geometry("600x900")
+root.title("Calculator")
+root.config(bg="orange")
+
+scvalue = StringVar()
+scvalue.set("")
+screen = Entry(root, textvar=scvalue, font="lucida 30 bold")
+screen.pack(fill=X, ipadx=8, pady=10, padx=10)
 
 
-    # Picking an operation
-    if operation == '+':
-        print('{} + {} = '.format(number_1, number_2))
-        print(number_1 + number_2)
-
-    elif operation == '-':
-        print('{} - {} = '.format(number_1, number_2))
-        print(number_1 - number_2)
-
-    elif operation == '*':
-        print('{} * {} = '.format(number_1, number_2))
-        print(number_1 * number_2)
-
-    elif operation == '/':
-        print('{} / {} = '.format(number_1, number_2))
-        print(number_1 / number_2)
+for i in range(9 ,0, -3):
+    f = Frame(root, bg="grey")
+    
+    for i in range(i, i-3, -1):
+        b = Button(f, text=str(i), padx=10, pady=5, font="lucida 35 bold")
+        b.pack(side=LEFT, padx=10, pady=5)
+        b.bind("<Button-1>", click)
+        
+    f.pack()
 
 
-    # Invalid operator
-    else:
-        print('You have not typed a valid operator, please run the program again.')
+f = Frame(root, bg="grey")
 
-    # Calculate again
-    # Add again() function to calculate() function
-    again()
+for char in "0-*":
+    b = Button(f, text=char, padx=10, pady=5, font="lucida 35 bold")
+    b.pack(side=LEFT, padx=13, pady=5)
+    b.bind("<Button-1>", click)
 
-def again():
-    calc_again = input('''
-Do you want to calculate again?
-Please type Y for YES or N for NO.
-''')
+f.pack()
 
-    # Yes or no?
-    if calc_again.upper() == 'Y':
-        calculate()
-    elif calc_again.upper() == 'N':
-        print('See you later.')
-    else:
-        again()
+f = Frame(root, bg="grey")
 
-calculate()
+for char in "/%=":
+    b = Button(f, text=char, padx=10, pady=5, font="lucida 35 bold")
+    b.pack(side=LEFT, padx=9, pady=5)
+    b.bind("<Button-1>", click)
+
+f.pack()
+
+f = Frame(root, bg="grey")
+
+b = Button(f, text="c", padx=10, pady=5, font="lucida 35 bold")
+b.pack(side=LEFT, padx=5, pady=5)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="clac", padx=10, pady=5, font="lucida 25 bold")
+b.pack(side=LEFT, padx=5, pady=5)
+
+
+b = Button(f, text="+", padx=10, pady=5, font="lucida 35 bold")
+b.pack(side=LEFT, padx=5, pady=5)
+b.bind("<Button-1>", click)
+f.pack()
+
+
+root.mainloop()
